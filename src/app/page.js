6788b8347d889cce582a6963699965f5a9b84f42
JonatasNP <jonatas.nicolau@academico.ifpb.edu.br>
2024-08-20 07:41:21 -0300

    ass

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,41 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+
+import ImgLogo from './imgs/logo-apae.png';
+import Barra from "./icones/icone-barra.png";
+
+export default function Inicial() {
   return (
+
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="App-Inicial" id={styles.cobertura1}>
+        <div id={styles.cobertura2}>
+          <div className={styles.saudacoes}>
+              <h1>BEM-VINDO(A)!</h1>
+              <p>Para cadastrar-se, preencha as informações ao lado.</p>
+              <p>Caso você já esteja cadastrado, clique no botão de Login.</p>
+              <Image src={Barra} alt="Barra Decorativa" style={{width: "35vw", height: "3vh"}} />
+              <p><button id={styles.buttonInicial}>Login</button></p>
+          </div>
+  
+          <div className={styles.cadastro}>
+              <Image src={ImgLogo} id={styles.imgLogo} alt="Logo da CadAPAE"></Image>
+              <h1 className={styles.tituloCadastro}>CadAPAE</h1>
+  
+              <h1 id={styles.titulo}>CRIAR CONTA</h1>
+  
+              <p><input placeholder="NOME" id={styles.inputNome} type="text" className={styles.InputInicial}></input></p>
+              <p><input placeholder="E-MAIL" id={styles.inputEmail} type="email" className={styles.InputInicial}></input></p>
+              <p><input placeholder="TELEFONE" id={styles.inputTelefone} type="tel" name="telefone" pattern="[0-9]{2}-[0-9]{4}-[0-9]{4}" className={styles.InputInicial}></input></p>
+              <p><input placeholder="SENHA" id={styles.inputSenha} type="password" className={styles.InputInicial}></input></p>
+  
+              <p><button id={styles.buttonInicial}>Entrar</button></p>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+
   );
 }
+
